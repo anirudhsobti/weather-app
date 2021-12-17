027d50fc1ac1c3707659c5c7b4ad5c154934b912
Anirudh Sobti <anirudh.sobti2000@gmail.com>
2021-12-17 20:05:14 +0530

    second commit

--- request.js
+++ request.js
@@ -3,7 +3,7 @@ const key = '11628e08632e37f3a267bc3efa90dc4c';
 
 
   const requestCity = async(city)=>{
-      const baseurl = "http://api.openweathermap.org/data/2.5/weather";
+      const baseurl = "https://api.openweathermap.org/data/2.5/weather";
       const query = `?q=${city}&appid=${key}`;
 
       const response = await fetch(baseurl+query);
